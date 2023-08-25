9417bb7aaec1613401eaaf8418031654bd9b4b0e
MarcinWypior <marcinwypior1990@gmail.com>
2023-08-25 12:05:13 +0200

    Display 4 versions of he same frame

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame =cap.read()
-    print(ret,frame)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
 
-    cv2.imshow("frame",frame)
+
+    image = np.zeros(frame.shape, np.uint8)
+    smaller_frame = cv2.resize(frame,(0,0),fx=0.5,fy=0.5)
+    image[:height//2,:width//2] = cv2.rotate(smaller_frame,cv2.ROTATE_180)
+    image[height//2:height,:width//2] = cv2.rotate(smaller_frame,cv2.ROTATE_180)
+    image[:height//2,width//2:width] = cv2.rotate(smaller_frame,cv2.ROTATE_180)
+    image[height//2:height,width//2:width] = smaller_frame
+
+    cv2.imshow("frame",image)
 
     if cv2.waitKey(1) == ord('q'):
         break
